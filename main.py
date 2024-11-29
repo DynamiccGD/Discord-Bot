66917ba7a1f6b30a5cc2c7e199f7f93a4d0340e9
Dynamic <dynamiccgd@outlook.com>
2024-11-28 23:54:07 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands, tasks
+import os
+import asyncio
+from itertools import cycle
+
+
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
+bot_statuses = cycle(["Geometry Dash", "Beat Saber", "Planet Coaster", "Roblox"])
+
+@tasks.loop(seconds=1800)
+async def change_bot_status():
+    await bot.change_presence(activity=discord.Game(next(bot_statuses)))
+
+
+@bot.event
+async def on_ready():
+    print("bot ready")
+    change_bot_status.start()
+
+with open("token.txt") as file:
+    token = file.read()
+
+
+
+async def Load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with bot:
+        await Load()
+        await bot.start(token)
+
+asyncio.run(main())
